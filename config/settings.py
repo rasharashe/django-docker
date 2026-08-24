c5317cd6950b8c8f4dac060199238aba93130b14
Rashad <rashadrashe@outlook.com>
2026-08-24 22:59:39 +0530

    Allow ALB health checks

--- config/settings.py
+++ config/settings.py
@@ -27,9 +27,7 @@ SECRET_KEY = 'django-insecure-w+e7!%5(_i#hc27o4u!nei3-az^k&xnd@4gaok!s-homt!fk^i
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    "15.206.92.117",
-    "localhost",
-    "127.0.0.1",
+    '*',
 ]
 
 
